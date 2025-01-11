aa6a4c967e3e7ca994ebd47023fa2b51ffe71bc0
aman-maharshi <amanm4harshi@gmail.com>
2025-01-11 17:31:18 +0530

    faq one added

--- src/FaqOne.jsx
+++ src/FaqOne.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { SlideDown } from 'react-slidedown'
+import 'react-slidedown/lib/slidedown.css'
 
 const App = () => {
   const [expand, setExpand] = useState(false)
@@ -16,12 +18,13 @@ const App = () => {
         <div className={`w-[17px] h-1 bg-[#0d3332] absolute right-4 top-[50%] translate-x-[-50%] z-10 transition-transform duration-300 rounded-sm ${expand ? 'rotate-90' : ''}`}></div>
         <div className='w-[17px] h-1 bg-[#0d3332] absolute right-4 top-[50%] translate-x-[-50%] z-20 rounded-sm'></div>
       </div>
-
-      {expand && (
-        <div className='p-4 text-lg'>
-          This iconic line by Yoda means that success requires complete commitment. Half-hearted attempts or excuses won’t lead to mastery or results. Yoda is teaching Luke the importance of belief in oneself and the focus required to achieve greatness.
-        </div>
-      )}
+      <SlideDown className={'my-dropdown-slidedown'}>
+        {expand && (
+          <div className='p-4 text-lg'>
+            This iconic line by Yoda means that success requires complete commitment. Half-hearted attempts or excuses won’t lead to mastery or results. Yoda is teaching Luke the importance of belief in oneself and the focus required to achieve greatness.
+          </div>
+        )}
+      </SlideDown>
     </div>
   )
 }
